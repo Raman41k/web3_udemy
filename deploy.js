60961b5389433dcb6bf251e82652774f76d57b95
Roman <romangugel@gmail.com>
2024-09-19 11:08:18 +0300

    first commit

---
+++ deploy.js
@@ -0,0 +1,30 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const { Web3 } = require('web3');
+const { interface, bytecode } = require('./compile');
+require('dotenv').config();
+
+const provider = new HDWalletProvider(
+    process.env.MNEMONIC_PHRASE,
+    process.env.INFURA_HOLESKY_API_KEY,
+    0,
+    1
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    try {
+        const accounts = await web3.eth.getAccounts();
+
+        console.log('account', accounts[0]);
+        
+        const result = await new web3.eth.Contract(JSON.parse(interface))
+            .deploy({ data: bytecode, arguments: ['Hello again!'] })
+            .send({ gas: '1000000', from: accounts[0] });
+    
+        console.log('contract', result.options.address);
+        provider.engine.stop();
+    } catch (e) {
+        console.log(e)
+    }
+};
+deploy();
